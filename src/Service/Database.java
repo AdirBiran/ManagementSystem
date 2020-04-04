4d994015f6ba9294ee489c6080f50e8cc36bb7dd
liatico <liatp@post.bgu.ac.il>
2020-04-04 14:12:07 +0300

    added games to the database and the functionality with it -getFutureGames, getAllGames also search user by type - returns all users of a specific type

--- src/Service/Database.java
+++ src/Service/Database.java
@@ -1,7 +1,10 @@
 package Service;
 import Domain.*;
-
+import java.util.Date;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Calendar;
 
 
 public class Database //maybe generalize with interface?
@@ -9,11 +12,13 @@ public class Database //maybe generalize with interface?
     private HashMap<String, String> userNamesAndPasswords; //- <userId, encryptedPassword??>
     private HashMap<String,User> usersInDatabase; // - <userId,User>
     private HashMap<String,Asset> assetsInDatabase;// - <asset.name, Asset>
+    private HashMap<String, Game> gamesInDatabase; // - <game.id, Game>
 
     public Database() {
         userNamesAndPasswords = new HashMap<>();
         usersInDatabase = new HashMap<>();
         assetsInDatabase = new HashMap<>();
+        gamesInDatabase = new HashMap<>();
     }
     /*
     this function gets a name of an asset and returns a pointer to the object of this asset
@@ -30,6 +35,31 @@ public class Database //maybe generalize with interface?
      */
     public User getUser(String userId){
         return (User)search("User", userId);
+    }
+    /*
+    this function gets a gameId - Game.toString (its address in memory) and returns a pointer to the object of this game
+    return null if cant find game
+    */
+    public Game getGame(String gameId){
+        return (Game)search("Game", gameId);
+    }
+    /*
+    this function returns all games in database
+     */
+    public LinkedList<Game> getAllGames(){
+        return new LinkedList(gamesInDatabase.values());
+    }
+    /*
+    this function returns a list of all future games
+     */
+    public LinkedList<Game> getAllFutureGames(){
+        Date today = Calendar.getInstance().getTime();
+        LinkedList<Game> futureGames = new LinkedList<>();
+        for(Game game : gamesInDatabase.values()){
+            if(today.before(game.getDate()))
+                futureGames.add(game);
+        }
+        return futureGames;
     }
     /*
     adds an asset to the database
@@ -57,6 +87,16 @@ public class Database //maybe generalize with interface?
         return false;
     }
     /*
+    this function adds a game to the database according to its toString() value
+    returns false if the game already exists
+    */
+    public boolean addGame(Game game){
+        if(gamesInDatabase.containsKey(game.toString()))
+            return false;
+        gamesInDatabase.put(game.toString(), game);
+        return true;
+    }
+    /*
     this function perform a authentication check for username an password
     returns true if this is the correct credentials and false otherwise
      */
@@ -68,23 +108,100 @@ public class Database //maybe generalize with interface?
         }
         return false;
     }
+    /*
+    this function returns a list of users of a specific type. for example all admins, all players ext.
+    the input is a string of the type "Admin", "Player"
+    if there aren't any users of this type - the list will be empty
+    if the string type is wrong the function will return null
+     */
+    public List<User> getListOfAllSpecificUsers(String userType){
+        LinkedList<User> listOfUsers = new LinkedList<>();
+        switch(userType){
+            case("Admin"):{
+                for(User user : usersInDatabase.values()){
+                    if(user instanceof Admin)
+                        listOfUsers.add(user);
+                }
+                return listOfUsers;
+
+            }
+            case("Coach"):{
+                for(User user : usersInDatabase.values()){
+                    if(user instanceof Coach)
+                        listOfUsers.add(user);
+                }
+                return listOfUsers;
+
+            }
+            case("Fan"):{
+                for(User user : usersInDatabase.values()){
+                    if(user instanceof Fan)
+                        listOfUsers.add(user);
+                }
+                return listOfUsers;
+
+            }
+            case("Player"):{
+                for(User user : usersInDatabase.values()){
+                    if(user instanceof Player)
+                        listOfUsers.add(user);
+                }
+                return listOfUsers;
+            }
+            case("Referee"):{
+                for(User user : usersInDatabase.values()){
+                    if(user instanceof Referee)
+                        listOfUsers.add(user);
+                }
+                return listOfUsers;
+            }
+            case("TeamManager"):{
+                for(User user : usersInDatabase.values()){
+                    if(user instanceof TeamManager)
+                        listOfUsers.add(user);
+                }
+                return listOfUsers;
+            }
+            case("TeamOwner"):{
+                for(User user : usersInDatabase.values()){
+                    if(user instanceof TeamOwner)
+                        listOfUsers.add(user);
+                }
+                return listOfUsers;
+            }
+            case("UnionRepresentative"):{
+                for(User user : usersInDatabase.values()){
+                    if(user instanceof UnionRepresentative)
+                        listOfUsers.add(user);
+                }
+                return listOfUsers;
+            }
+        }
+        return null;
+    }
 
 
     private Object search(String whatType, String searchWord){
-        if(Checker.isValid(searchWord)){
-            Object result = null;
             switch(whatType){
                 case("Asset"):{
                     for(String nameOfAsset : assetsInDatabase.keySet()) {
                         if (searchWord.equals(nameOfAsset))
-                            result = assetsInDatabase.get(searchWord);
+                            return assetsInDatabase.get(searchWord);
                     }
                     break;
                 }
                 case("User"):{
                     for(String userId : usersInDatabase.keySet()) {
-                        if (searchWord.equals(userId))
-                            result = usersInDatabase.get(searchWord);
+                        if (searchWord.equals(userId)) {
+                            return usersInDatabase.get(searchWord);
+                        }
+                    }
+                    break;
+                }
+                case("Game"):{
+                    for(String gameId:gamesInDatabase.keySet()){
+                        if(searchWord.equals(gameId))
+                            return gamesInDatabase.get(gameId);
                     }
                     break;
                 }
@@ -94,9 +211,8 @@ public class Database //maybe generalize with interface?
                 }
 
             }
-            return result;
-        }
-        return null;
+            return null;
+
     }
 
 }
