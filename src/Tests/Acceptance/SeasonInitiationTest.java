cf41f099a5ffd14e37ecf39ab01fb61f9c9b2287
AdirBiran <adir.biran@gmail.com>
2020-04-17 15:11:26 +0300

    Acceptance Tests added
    SystemInitiationTest
    SeasonInitiationTest
    TeamManagementTest

---
+++ src/Tests/Acceptance/SeasonInitiationTest.java
@@ -0,0 +1,47 @@
+package Tests.Acceptance;
+
+import Presentation.FootballManagementSystem;
+import org.junit.Test;
+
+public class SeasonInitiationTest {
+
+    @Test
+    public void defineLeagueTest_55()
+    {
+        FootballManagementSystem system = new FootballManagementSystem();
+        system.systemInit(true);
+        system.dataReboot();
+
+
+    }
+
+    @Test
+    public void defineLeagueTest_56()
+    {
+
+    }
+
+    @Test
+    public void defineLeagueTest_57()
+    {
+
+    }
+
+    @Test
+    public void defineLeagueTest_58()
+    {
+
+    }
+
+    @Test
+    public void setReferees_65()
+    {
+
+    }
+
+    @Test
+    public void setReferees_66()
+    {
+
+    }
+}
