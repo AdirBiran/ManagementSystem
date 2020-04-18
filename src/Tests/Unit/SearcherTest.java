e880785b3df8b7e96ce9429f5f4f1534e4353b71
shirbendor <48984898+shirbendor@users.noreply.github.com>
2020-04-18 21:08:24 +0300

    unit tests

--- src/Tests/Unit/SearcherTest.java
+++ src/Tests/Unit/SearcherTest.java
@@ -1,5 +1,8 @@
 package Tests.Unit;
 
+import Domain.Fan;
+import Domain.Searcher;
+import Service.FootballManagementSystem;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,13 +11,13 @@ public class SearcherTest {
 
     @Test
     public void searchInfo() {
+        FootballManagementSystem system;
+        system = new FootballManagementSystem();
+        system.systemInit(true);
+        system.dataReboot();
+        Fan fan = new Fan("AviLevi@gmail.com", "Avi", "Levi", "0500004544", "Yuda123");
+        Searcher searcher= new Searcher(system.getDatabase());
+        assertNotEquals(0,searcher.searchInfo(fan,"team0").size(),0);
     }
 
-    @Test
-    public void viewInfoAbout() {
-    }
-
-    @Test
-    public void viewSearchHistory() {
-    }
 }
