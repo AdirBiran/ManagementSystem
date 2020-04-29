649684ee9e480f2bb5bbfea6b13eb0b51c370ac7
shirbendor <48984898+shirbendor@users.noreply.github.com>
2020-04-29 23:01:48 +0300

    test

--- src/Domain/UnionBudget.java
+++ src/Domain/UnionBudget.java
@@ -5,11 +5,8 @@ public class UnionBudget {
     private Budget budget;
 
     public UnionBudget() {
-<<<<<<< HEAD
         //budget = new Budget(null);
-=======
         budget = new Budget();
->>>>>>> a3dabb2a87d72bd10ba1205f11d32143ebb45d37
     }
 
     public boolean addPayment(double income){
--- src/Test/UnitTest/TeamManagerTest.java
+++ src/Test/UnitTest/TeamManagerTest.java
@@ -1,32 +1,48 @@
 package UnitTest;
 
+import Domain.LeagueInSeason;
+import Domain.Team;
 import Domain.TeamManager;
+import Service.FootballManagementSystem;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class TeamManagerTest {
+ public class TeamManagerTest {
+
+    TeamManager teamManager;
+    FootballManagementSystem system;
+    Team team;
+    @Before
+    public void init(){
+        teamManager=new TeamManager("123456789",20000,false,false);
+        system = new FootballManagementSystem();
+        system.systemInit(true);
+        LeagueInSeason league = system.dataReboot();
+        team = league.getTeams().get(0);
+    }
 
     @Test
     public void getID() {
-        TeamManager teamManager=new TeamManager("123456789",20000,false,false);
         assertEquals(teamManager.getID(),"123456789");
     }
 
     @Test
-    public void deactivate() {
-    }
-
-    @Test
     public void getPrice() {
+        assertEquals(teamManager.getPrice(),20000,1);
     }
 
     @Test
     public void setPrice() {
+        teamManager.setPrice(30);
+        assertEquals(teamManager.getPrice(),30,1);
+
     }
 
     @Test
     public void getTeams() {
+        assertEquals(teamManager.getTeams().size(),0);
     }
 
     @Test
@@ -39,6 +55,8 @@ public class TeamManagerTest {
 
     @Test
     public void addTeam() {
+        teamManager.addTeam(team);
+        assertEquals(teamManager.getTeams().size(),1);
     }
 
     @Test
