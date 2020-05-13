0469012cb3d6cd5f0c7abc087162a9368f726106
shirbendor <48984898+shirbendor@users.noreply.github.com>
2020-05-13 21:30:02 +0300

    stubs

--- src/Service/StubIsraeliTaxLawsSystem.java
+++ src/Service/StubIsraeliTaxLawsSystem.java
@@ -5,4 +5,8 @@ public class StubIsraeliTaxLawsSystem {
     public boolean connect(boolean flag) {
         return flag;
     }
+
+    public double revenueAmount(double getTaxRate) {
+        return getTaxRate;
+    }
 }
