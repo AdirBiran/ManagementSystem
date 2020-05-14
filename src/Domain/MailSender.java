949b7c7e8d6dab3e3771482995250b13fa7af4e3
liatico <liatp@post.bgu.ac.il>
2020-05-14 19:54:24 +0300

    mail sender

--- src/Domain/MailSender.java
+++ src/Domain/MailSender.java
@@ -1,53 +1,33 @@
 package Domain;
 
-import java.util.*;
-import javax.mail.*;
-import javax.mail.internet.*;
-import javax.activation.*;
 
 public class MailSender {
 
-    public static boolean send(String mail, String message){
-/*
-        String from = "liatico77@gmail.com";
+    private static boolean reallySend =true;
 
-        // Assuming you are sending email from localhost-check this
-        String host = "localhost";
+    public static boolean send(String to, String messageToSend){
+        if(reallySend){
+            String mailSenderFile = "./MailSender/MailSender.exe";
 
-        // Get system properties
-        Properties properties = System.getProperties();
-
-        // Setup mail server
-        properties.setProperty("mail.smtp.host", host);
-
-        // Get the default Session object.
-        Session session = Session.getDefaultInstance(properties);
-
-        try {
-            // Create a default MimeMessage object.
-            MimeMessage mimeMessage = new MimeMessage(session);
-
-            // Set From: header field of the header.
-            mimeMessage.setFrom(new InternetAddress(from));
-
-            // Set To: header field of the header.
-            mimeMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(mail));
-
-            // Set Subject: header field
-            mimeMessage.setSubject("This is the Subject Line!");
-
-            // Now set the actual message
-            mimeMessage.setText(message);
-
-            // Send message
-            Transport.send(mimeMessage);
-            return true;
-
-        } catch (MessagingException mex) {
-            mex.printStackTrace();
-            return false;
+            try
+            {
+                String subject = "New message from Team 5 Football Management System" ;
+                new ProcessBuilder(mailSenderFile,to,subject,messageToSend).start();
+                return true;
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+                return false;
+            }
         }
-*/
         return true;
+
     }
+
+
+    public static void setReallySend(boolean reallySend) {
+        MailSender.reallySend = reallySend;
+    }
+
 }
