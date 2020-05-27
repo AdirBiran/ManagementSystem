9d4536fe8378d290652e65307df7ac3382699637
shirbendor <48984898+shirbendor@users.noreply.github.com>
2020-05-28 00:52:26 +0300

    tests+referee

--- src/main/java/Test/AcceptanceTest/EventsGameTest.java
+++ src/main/java/Test/AcceptanceTest/EventsGameTest.java
@@ -17,7 +17,7 @@ public class EventsGameTest {
     RefereeSystem refereeSystem;
     FootballManagementSystem system;
     Admin admin;
-    Referee referee;
+   // Referee referee;
     Game game;
     User mesi;
     @Before
@@ -30,7 +30,7 @@ public class EventsGameTest {
         LeagueInSeason league =system.getDatabase().getAllLeaguesInSeasons().get(0);
         admin = (Admin) system.getAdmin();
         game=league.getGames().get(0);
-        referee=game.getMainReferee();
+      //  referee=game.getMainReferee();
         mesi = admin.addNewPlayer("mesi", "mesi", "mesi@mail.com", new Date(30 / 5 / 93), Player.RolePlayer.goalkeeper, 200000);
 
     }
@@ -38,11 +38,16 @@ public class EventsGameTest {
     @Test
     public void eventsGameSuccess_72()
     {
-       assertTrue(refereeSystem.addEventToGame(referee.getID(),game.getId(), "RedCard",game.getHostTeam().getPlayers().get(0).getID(),game.getHostTeam().getID()));
+       assertTrue(refereeSystem.addEventToGame(game.getMainReferee().getID(),game.getId(), "RedCard",game.getHostTeam().getPlayers().get(0).getID(),game.getHostTeam().getID()));
     }
     @Test
     public void eventsGameFail_73()
     {
-        assertFalse(refereeSystem.addEventToGame(referee.getID(),game.getId(), "RedCard",game.getHostTeam().getPlayers().get(0).getID(),null));
+        assertFalse(refereeSystem.addEventToGame(game.getMainReferee().getID(),game.getId(), "RedCard",game.getHostTeam().getPlayers().get(0).getID(),null));
+    }
+    @Test
+    public void changeEventsGame_74()//check about the 5h
+    {
+        assertTrue(refereeSystem.changeEvent(game.getMainReferee().getID(),game.getId(),game.getEventReportString().get(0),"new"));
     }
 }
